1a32d24a4c67b92b746fd07c09c4606c982851c0
b2ue <abiturium+github@googlemail.com>
2015-07-31 15:43:39 +0200

    List View mit Fav Button

--- app/js/personList.js
+++ app/js/personList.js
@@ -1,15 +1,37 @@
+//$.getScript("/stub.js", function(){
+//    alert("Script loaded but not necessarily executed.");
+//});
+
+
+
 //Für jede Person wird eine neue Page Detailseite angelegt
-var personArray = ["Person1", "Person2", "Person3", "Person4"];
+//var personArray = ["Person1", "Person2", "Person3", "Person4"];
+
+//alert(speaker1.getName());
+//alert("hello");
+
+
 
 //array durchlaufen listView mit den Personen populieren. Für jede Person Detailseite anlegen
-$.each(personArray, function( index, value ) {
+$.each(demoSpeakerArray, function( index, personArrayValue ) { //demoSpeakerArray aus stub.js
 
-    //TODO: Anforderung unique ID sein
-    var person = value;
-    //ListView population
-    $("body").append('<div data-role="page" id="' + person + '">' +
-        '<div data-role="header"><h1>' + person + '</h1></div>');
+    var personId = personArrayValue.getId();
+    var personName = personArrayValue.getName();
+    var personFirma = personArrayValue.getOrganization();
+    var personPosition = personArrayValue.getPosition();
+    var personPhoto = personArrayValue.getPhoto();
+
+    var pageLink = personId + personName;
 
     //Detailseiten anlegen
-    $("#personListView").append('<li><a href="#'+ person + '"> <img src="images/person.png" >'+ person + '  </a> </li>');
+    $("body").append('<div data-role="page" id="' + pageLink + '">' +
+        '<div data-role="header"><h1>' + personName + ' (' + personPosition + ')</h1></div>');
+
+    //ListView population
+    $("#personListView").append('<li>' +
+        '<a href="#'+ pageLink + '" class="">' +
+        '<div style="background-image: url(' + personPhoto + ')" class="personPhoto"></div>' +
+        '<div class="personText">'+ personName + ' (' + personPosition + ')<br />' + personFirma + '</div>' +
+        '</a>' +
+        '<a href="#test"></a></li>'); //<img src="' + personPhoto + '">
 });
