c832a1a5bec5d967f420227ce28546080441357d
Lukas Schaller <lukas.schaller@mni.thm.de>
2015-07-31 09:54:15 +0200

    Init Commit

---
+++ ConferenceApp/ConferenceApp/ConferenceApp.Shared/js/user.js
@@ -0,0 +1,4 @@
+﻿WinJS.UI.processAll().done(function () {
+    var splitView = document.querySelector(".splitView").winControl;
+    WinJS.UI._WinKeyboard(splitView.paneElement); // Temporary workaround: Draw keyboard focus visuals on NavBarCommands
+});
