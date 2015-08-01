fa1a9879615be4ead004a7961ebb560099a54cd0
Alexander (AxxG) <alexander@axxg.de>
2015-08-01 03:14:44 +0200

    Aktueller Stand

--- final/js/extend.js
+++ final/js/extend.js
@@ -9,11 +9,46 @@ function extendPages(){
 	addMenu($("#dashboardPage"), "dashboardPM");
 	addMenu($("#sessionListPage"), "sessionListPM");
 	addMenu($("#speakerListPage"), "speakerListPM");
+	
+	
 }
-
-
-
 
 function addMenu(body, id) {
-	$(body).append('<div data-role="popup" id="'+ id + '"><ul data-role="listview" data-inset="true" style="min-width: 210px;"><li data-role="list-divider">Choose an action</li><li><a href="#">View details</a></li><li><a href="#">Edit</a></li><li><a href="#">Disable</a></li><li><a href="#">Delete</a></li></ul></div>');
+	$(body).append('<div data-role="popup" id="'+ id + '"><ul data-role="listview" data-inset="true" style="min-width: 210px;"><li><a href="#dashboardPage">Dashboard</a></li><li><a href="#sessionListPage">Sessionplan</a></li><li><a href="#speakerListPage">Speakerübersicht</a></li></ul></div>');
 }
+
+
+// Seitenspezifisch
+
+
+
+$(document).on("pagecreate", "#speakerListPage", function(event){
+	var speakerArray = konfModel.getSpeakerArray();
+	$.each(speakerArray, function( index, speaker ) { //demoSpeakerArray aus stub.js
+        var speakerId = speaker.getId();
+        var speakerName = speaker.getName();
+        var speakerFirma = speaker.getOrganization();
+        var speakerPosition = speaker.getPosition();
+        var speakerPhoto = speaker.getPhoto();
+
+        //ListView population
+        $("#speakerListView").append('<li>' +
+            '<a onclick="goToSpeakerDetail(\'' + speakerId + '\');">' +
+            '<div style="background-image: url(' + speakerPhoto + ')" class="speakerPhoto"></div>' +
+            '<div class="speakerText">'+ speakerName + ' (' + speakerPosition + ')<br />' + speakerFirma + '</div>' +
+            '</a>' +
+            '<a></a></li>'); 
+    });
+
+    $("#speakerListView").listview( "refresh" );
+});
+
+$(document).on("pagehide", "#speakerListPage", function(event){
+	// notfalls cleanen
+});
+
+function goToSpeakerDetail(ident) {
+	alert(ident);	
+}
+
+
