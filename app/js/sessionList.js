537364e635ebc83eb14e6d09ff09b939b6898e65
b2ue <abiturium+github@googlemail.com>
2015-08-01 04:19:19 +0200

    Merge branch 'master' of https://github.com/ocdata/winjs into masterLocal
    Conflicts:
    	app/speaker.html
    
    Augen zu

--- app/js/sessionList.js
+++ app/js/sessionList.js
@@ -25,61 +25,89 @@ $.each(demoSessionArray, function (index, sessionArrayValue) { //demolinkarray a
     speakerString = speakerString.substr(0, speakerString.length - 2);
 
     var sessionPageLink = id + title;
-    sessionPageLink = sessionPageLink.replace(/\s+/g, '');
+    sessionPageLink = onlyNumbersAndLetters(sessionPageLink);
     
     //listview population
     $("#sessionListView").append('<li>' +
         '<a href="#' + sessionPageLink + '" class="">' +
-
         '<div class="linktext">' + title + '</div>' +
         '<div class="speakerString">' + speakerString + '</div>' +
-
         '<div class="speakLanguage">Vortrag in ' + speakerLanguage + '</div>' +
         '<div class="speakTime">(' + vonBis +')</div>' +
         '<div class="speakLocation">Ort: ' + speakLocation + '</div>' +
         '</a>' +
-        '<a href="#fav" class="fav" id="' + sessionPageLink+ '"></a></li>');
-
+        '<a href="#fav" id="'+ sessionPageLink +'" class="fav ' + sessionPageLink+ '" data-theme="c"></a></li>');
 
     //Detailseiten anlegen
     $("body").append('<div data-role="page" id="' + sessionPageLink + '">' +
         '<div data-role="header"><h1>' + title + ' (' + vonBis + ')</h1>' +
         '</div>');
+
 });
 
-//auf Fav klicken
-//$('.fav').click(function(){
-//    var sessionIdAndName = $(this).attr('id');
-//
-////    if(!$.cookie("favArray").empty()){
-//        var favs = [
-//            { 'name' : 'Abel', 'age' : 1 },
-//            { 'name' : 'Bella', 'age' : 2 },
-//            { 'name' : 'Chad', 'age' : 3 },
-//        ];
-//    Cookies.set("favArray", JSON.stringify(favs));
-////    }
-//});
-
-var addNewFav = function (fav) {
-    // holen und befüllen oder neues erstellen
-    var favSessions = JSON.parse(localStorage.getItem('favoriteSessions')) || [];
-
-    favSessions.push({sessions: fav});
-    // zurückschreiben
-    localStorage.setItem('favoriteSessions', JSON.stringify(favSessions));
+function onlyNumbersAndLetters(string){
+    if(string){
+        return string.replace(/[^a-zA-Z0-9_]/g,'');
+    }
+    return;
 }
 
 
+var favChanged = function (favId) {
+    favId = onlyNumbersAndLetters(favId);
+    // Inhalt aus localstorage holen als Json
+    var favSessions = JSON.parse(localStorage.getItem('favoriteSessions')) || [];
+    //if favId is in inArray returns the index of favId or -1
+    if($.inArray(favId, favSessions) >= 0) {
+        setFavoriteIcon(favId, false);
+        //remove element
+        favSessions.splice(favSessions.indexOf(favId), 1);
+    } else {
+        setFavoriteIcon(favId, true);
+        //Element dazupushen
+        favSessions.push(favId);
+    }
+    //favSessions zurückschreiben
+    localStorage.setItem('favoriteSessions', JSON.stringify(favSessions));
+}
+var setFavoriteIcon = function(id, isFavorite){
+    if(isFavorite)
+        $('.' + id).buttonMarkup({theme: 'b'});
+    else
+        $('.' + id).buttonMarkup({theme: 'c'});
+}
+var markFavoriteSessionsWithIcon = function(){
+    var favSessions = JSON.parse(localStorage.getItem('favoriteSessions'));
+
+//    document.styleSheets[0].insertRule('.session-3DeutscheBank:after{ content: "\f004";}', 1);
+
+    $.each(favSessions, function (index, favId) {
+
+        favId = onlyNumbersAndLetters(favId);
+        //#session-3DeutscheBank::after
+//        document.styleSheets[0].insertRule('.' + favId + ':after{ content: "\f004";}', 1);
+
+        $('.' + favId).buttonMarkup({theme: 'b'});
+
+
+//        $('#' + favId + '::after').css('content', '\f004');
+    });
+
+}
+markFavoriteSessionsWithIcon();
 
 $('.fav').click(function(){
-    alert("test");
+//    alert("");
     //use HTML5 LocalStorage
     var sessionIdAndName = $(this).attr('id');
 
-    addNewFav(sessionIdAndName);
+    favChanged(sessionIdAndName);
+
 
 });
+
+
+
 // Check browser support
 //if (typeof(Storage) != "undefined") {
 //    // Store
