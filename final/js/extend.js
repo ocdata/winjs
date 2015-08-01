27b0788f9f78f8dbe1e5e198734021a415e4e712
Alexander (AxxG) <alexander@axxg.de>
2015-08-01 08:07:40 +0200

    SpeakerDetail

--- final/js/extend.js
+++ final/js/extend.js
@@ -19,6 +19,84 @@ function addMenu(body, id) {
 	$(body).append('<div data-role="panel" id="'+ id + '" data-position="left" data-display="overlay" ><h3>Menü</h3><ul data-role="listview" data-inset="true" style="min-width: 210px;"><li><a href="#dashboardPage">Dashboard</a></li><li><a href="#sessionListPage">Sessionplan</a></li><li><a href="#speakerListPage">Speakerübersicht</a></li></ul><br/><a data-rel="close" class="ui-btn">schließen</a></div>');
 }
 
+function genLinkList(listView, linkArray) {
+	// liste erstmal reseten
+	listView.empty();
+	
+	 $.each(linkArray, function(index, link) {
+
+		    var url = link.getUrl();
+		    var title = link.getTitle();
+		    var service = link.getService();
+
+		    //listview population
+		    listView.append('<li>' +
+		      '<a href="' + url + '" target="_blank">' +
+		      '<div class="linktext">' + title + ' (' + service + ')</div>' +
+		      '</a></li>'); 
+	  });
+	 
+	 try {
+		 // Liste neuerstellen lassen
+		 listView.listview('refresh');
+	} catch (e) {
+		// TODO: handle exception
+	}
+}
+
+function genSessionList(listView, sessionArray) {
+	// liste erstmal reseten
+	listView.empty();
+	
+	$.each(sessionArray, function (index, session) { 
+
+	    var ident = session.getId();
+	    var title = session.getTitle();
+	    var sessionRemoteUrl = session.getUrl();
+	    var begin = session.getBegin();
+	    var end = session.getEnd();
+	    var speakerArray = session.getSpeakerArray();
+	    var speakerLanguage = session.getLanguageObj().getLabelDe();
+	    var speakLocation = session.getLocationObj().getLabelDe();
+
+	    begin = moment(begin).format("HH:mm");
+	    end = moment(end).format("HH:mm");
+
+	    var vonBis = begin + ' - ' + end;
+
+	    var speakerString = "";
+	    $.each(speakerArray, function (index, speaker) {
+	        speakerString += speaker.getName() + ", ";
+	    });
+
+	    //letztes Komma abschneiden
+	    speakerString = speakerString.substr(0, speakerString.length - 2);
+
+	    
+	    //listview population
+	    listView.append('<li>' +
+	        '<a onclick="goToSessionDetail(\'' + ident + '\');">' +
+
+	        '<div class="linktext">' + title + '</div>' +
+	        '<div class="speakerString">' + speakerString + '</div>' +
+
+	        '<div>Vortrag in ' + speakerLanguage + '</div>' +
+	        '<div>' + vonBis +'</div>' +
+	        '<div class="sessionLocation">Ort: ' + speakLocation + '</div>' +
+	        '</a>' +
+	        '<a></a></li>');
+	});
+	
+	
+	 
+	 try {
+		 // Liste neuerstellen lassen
+		 listView.listview('refresh');
+	} catch (e) {
+		// TODO: handle exception
+	}
+}
+
 
 // Seitenspezifisch
 
@@ -48,7 +126,51 @@ $(document).on("pagehide", "#speakerListPage", function(event){
 });
 
 function goToSpeakerDetail(ident) {
-	alert(ident);	
+	// Speaker suchen
+	var speaker = null;
+	
+	for (var int = 0; int < konfModel.getSpeakerArray().length; int++) {
+		speaker = konfModel.getSpeakerArray()[int];
+		if(speaker.getId() == ident){
+			// richtiger Speaker gefunden oder den Letzten!
+			break;
+		}
+	}
+	
+	// Seite belegen
+	 $("#speakerDetailFoto").attr("src", speaker.getPhoto());
+	 $("#speakerDetailName").text(speaker.getName());
+	 $("#speakerDetailPosition").text(speaker.getPosition());
+	 $("#speakerDetailOrganisation").text(speaker.getOrganization());
+	 $("#speakerDetailBiografie").text(speaker.getBiography());
+	 
+	 //var test = new Speaker(id, name, photo, url, biography, organization, organizationUrl, position, linkArray);
+	 //test.getBiography()
+	 
+	 // Linkliste erzeugen
+	 genLinkList($("#speakerDetailLinkListView"), speaker.getLinkArray());
+	 
+	 // Sessionliste erzeugen
+	 // sonst zyklische Abhängigkeiten im Datenmodell!!!
+	 var speakerSessionArray = new Array();
+	 for (var int2 = 0; int2 < konfModel.getSessionArray().length; int2++) {
+		var session = konfModel.getSessionArray()[int2];
+		
+		for (var int3 = 0; int3 < session.getSpeakerArray().length; int3++) {
+			var sessionSpeaker = session.getSpeakerArray()[int3];
+			if(sessionSpeaker.getId() == speaker.getId()){
+				speakerSessionArray.push(session);
+				break;
+			}
+		}
+		
+	}
+	 
+	 // SessionListe erzeugen
+	 genSessionList($("#speakerDetailSessionListView"), speakerSessionArray);
+
+	
+	// Seite wechseln
 	$( ":mobile-pagecontainer" ).pagecontainer( "change", $("#speakerDetailPage"));
 }
 
